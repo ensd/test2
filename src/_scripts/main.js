ddae9ba315134cbf64f1fd2a41d18ac2fa8a2062
Егор Савенков <e.n.savenkov@gmail.com>
2020-03-21 01:58:04 +0300

    Update main.js

--- src/_scripts/main.js
+++ src/_scripts/main.js
@@ -1,31 +1,25 @@
 const $ = require('jquery');
 
 (function() {
-	function addToCartHanlder() {
-		let $btns = $('[data-add-to-cart]');
+	let $body = $('body');
 
-		$btns.on('click', function(e){
-			e.preventDefault();
-			
-			$.getJSON({
-				url: './fakedata/cartResponse.json',
-				success: (res) => {
-					if (res.success) {
-						$btns
-							.text('Добавлено')
-							.removeClass('btn-primary')
-							.addClass('btn-success');
-							
-						$('[data-cart-count]').text(res.count);
-					}
+	$body.on('click', '[data-add-to-cart]', function(e) {
+		let $this = $(this);
+
+		e.preventDefault();
+		
+		$.getJSON({
+			url: './fakedata/cartResponse.json',
+			success: (res) => {
+				if (res.success) {
+					$this
+						.text('Добавлено')
+						.removeClass('btn-primary')
+						.addClass('btn-success');
+						
+					$('[data-cart-count]').text(res.count);
 				}
-			});
+			}
 		});
-	};
-
-	function main() {
-		addToCartHanlder();
-	};
-
-	main();
+	});
 }());
